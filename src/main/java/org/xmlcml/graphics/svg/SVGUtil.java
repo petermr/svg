cefd17dacc016d263c81f03eca741076dc8c3e39
AndyHowlett <none@none>
2014-12-05 17:14:34 +0000

    Added imports (should have been part of last commit).

--- src/main/java/org/xmlcml/graphics/svg/SVGUtil.java
+++ src/main/java/org/xmlcml/graphics/svg/SVGUtil.java
@@ -13,7 +13,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SVGUtil {
 
