ee3b901450b2af6cbb52cf002d1b7dcd1b637759
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2017-06-24 07:24:16 +0100

    add rot90 and non-standard minus to SVGWord

--- src/main/java/org/xmlcml/graphics/svg/text/SVGWord.java
+++ src/main/java/org/xmlcml/graphics/svg/text/SVGWord.java
@@ -1,9 +1,13 @@
 package org.xmlcml.graphics.svg.text;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.xmlcml.euclid.Real2;
 import org.xmlcml.euclid.Real2Range;
+import org.xmlcml.euclid.Util;
 import org.xmlcml.graphics.svg.SVGElement;
 import org.xmlcml.graphics.svg.SVGG;
 import org.xmlcml.graphics.svg.SVGText;
@@ -20,7 +24,9 @@ import org.xmlcml.xml.XMLUtil;
 public class SVGWord extends SVGG {
 
 	
-	private static final char EMDASH = (char)8211;
+	private static final String ENDASH = "\\u2013"; // (char)8211;
+	private static final String FULL_MINUS = "\\u2212";
+	public final static List<String> NON_STANDARD_MINUS = Arrays.asList(new String[]{ENDASH, FULL_MINUS});
 	private static final double DELTA_Y_TEXT = 0.3;
 	private static final Logger LOG = Logger.getLogger(SVGWord.class);
 	static {
@@ -29,6 +35,7 @@ public class SVGWord extends SVGG {
 	
 	public static final String CLASS = "word";
 	private double interCharacterFactor = 0.1;
+	private boolean isRot90 = false; // simple approach avoiding rotating the whole text
 	
 	public SVGWord() {
 		super();
@@ -39,6 +46,11 @@ public class SVGWord extends SVGG {
 		this.appendChild(svgText.copy());
 	}
 
+	public SVGWord(SVGElement svgText, boolean isRot90) {
+		this.appendChild(svgText.copy());
+		this.setRot90(isRot90);
+	}
+
 	/** from Tesseract
 	 * 
 	 * @return
@@ -46,7 +58,10 @@ public class SVGWord extends SVGG {
 	public SVGText getSVGText() {
 		return (SVGText) XMLUtil.getSingleElement(this, "*[local-name()='"+SVGText.TAG+"']");
 	}
-	
+
+	public void setRot90(boolean r) {
+		this.isRot90 = r;
+	}
 	/**
 	 * gap between end of last word and start of this.
 	 * 
@@ -58,7 +73,19 @@ public class SVGWord extends SVGG {
 	public double gapFollowing(SVGWord lastWord) {
 		Real2Range lastBox = (lastWord == null) ? null : lastWord.getChildRectBoundingBox();
 		Real2Range thisBox = this.getChildRectBoundingBox();
-		return (lastBox == null || thisBox == null) ? 0.0 : thisBox.getXMin() - lastBox.getXMax();
+		return (lastBox == null || thisBox == null) ? 0.0 : getDistance(lastBox, thisBox);
+	}
+
+	private double getDistance(Real2Range lastBox, Real2Range thisBox) {
+		double dist = -Double.MAX_VALUE;
+		if (isRot90) {
+			dist = lastBox.getYMin() - thisBox.getYMax();
+		} else {
+			dist = thisBox.getXMin() - lastBox.getXMax();
+		}
+		dist = Util.format(dist, 3);
+		LOG.debug("dist: "+dist);
+		return dist;
 	}
 
 	/**
@@ -73,7 +100,7 @@ public class SVGWord extends SVGG {
 		if (nextText == null) return Double.NaN;
 		Real2Range nextBox = nextText.getBoundingBox();
 		Real2Range thisBox = this.getBoundingBox();
-		return nextBox.getXMin() - thisBox.getXMax();
+		return getDistance(thisBox, nextBox);
 	}
 
 	public Real2Range getBoundingBox() {
@@ -98,7 +125,8 @@ public class SVGWord extends SVGG {
 			return false;
 		}
 		Real2 deltaXY = this.getXY().subtract(text.getXY()); 
-		return Math.abs(deltaXY.getY()) < (DELTA_Y_TEXT * this.getFontSize());
+		double delta = DELTA_Y_TEXT * this.getFontSize();
+		return Math.abs(isRot90 ? deltaXY.getX() : deltaXY.getY()) < delta;
 	}
 
 	public void append(SVGText newText) {
@@ -138,15 +166,34 @@ public class SVGWord extends SVGG {
 		return text == null ? null : text.getValue();
 	}
 
-	public void emdashToMinus() {
+	public void normalizeMinus() {
 		String s = this.getStringValue();
-		if (s != null && s.contains(String.valueOf(EMDASH))) {
-			String s1 = s.replaceAll(String.valueOf(EMDASH), S_MINUS);
-			this.getSVGText().setText(s1);
-			if (!s.equals(s1)) {
-				LOG.debug("EMDASH: "+s+" => "+s1);
+		normalizeChars(s, S_MINUS, SVGWord.NON_STANDARD_MINUS);
+	}
+
+	private void normalizeChars(String s, String standardChar, List<String> nonStandardChars) {
+		if (s != null) {
+			String s0 = s;
+			s = replaceNonStandardChars(s0, standardChar, nonStandardChars);
+			if (!s.equals(s0)) {
+				this.getSVGText().setText(s);
+				LOG.debug("nonStandardMinus: "+s0+" => "+s);
 			}
 		}
+	}
+
+	/** replaces all occurrences of the nonStandardChars by the standardChar.
+	 * 
+	 * @param s
+	 * @param standardChar
+	 * @param nonStandardChars
+	 * @return
+	 */
+	public static String replaceNonStandardChars(String s, String standardChar, List<String>nonStandardChars) {
+		for (String nonStandardChar : nonStandardChars) {
+			s = s.replaceAll(nonStandardChar, standardChar);
+		}
+		return s;
 	}
 
 }
