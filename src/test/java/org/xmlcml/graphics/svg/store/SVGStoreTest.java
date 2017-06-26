c5cfebd10d5defab033aca893c280001a73750b9
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2017-06-26 12:06:59 +0100

    develop more tests and test data for axial recognition

--- src/test/java/org/xmlcml/graphics/svg/store/SVGStoreTest.java
+++ src/test/java/org/xmlcml/graphics/svg/store/SVGStoreTest.java
@@ -1,4 +1,4 @@
-package org.xmlcml.graphics.svg.plot;
+package org.xmlcml.graphics.svg.store;
 
 import java.io.File;
 import java.io.IOException;
@@ -7,6 +7,7 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.xmlcml.graphics.svg.Fixtures;
+import org.xmlcml.graphics.svg.plot.PlotBox;
 
 public class SVGStoreTest {
 	private static final Logger LOG = Logger.getLogger(SVGStoreTest.class);
@@ -17,11 +18,10 @@ public class SVGStoreTest {
 	@Test
 	public void testSingleFigure() throws IOException {
 		String fileRoot = "10.1186_s12885-016-2685-3_page7";
-		PlotBox plotBox = new PlotBox();
+		SVGStore store = new SVGStore();
 		File inputSVGFile = new File(Fixtures.FIGURE_DIR, fileRoot+".svg");
 		LOG.debug("reading: "+inputSVGFile);
-		plotBox.readAndCreateCSVPlot(inputSVGFile);
-		plotBox.writeProcessedSVG(new File("target/figure/"+fileRoot+".svg"));
+		store.readGraphicsComponents(inputSVGFile);
 	}
 
 }
